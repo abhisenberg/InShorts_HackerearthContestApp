48f9909d7868dbf81337f8a8e7e94508e0ca5fee
abhisenberg <ab96sh@gmail.com>
2017-09-09 10:13:18 +0530

    first empty commit

---
+++ app/src/main/java/com/example/abheisenberg/inshortscontest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.abheisenberg.inshortscontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
